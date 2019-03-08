ac6b95e7522d42d25df19d5b0ee4c08ba9160809
Richard Kyle Easterling <kyle.easterling@Richards-MacBook-Pro.local>
2019-03-08 10:25:01 -0600

    migrate checks over

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,16 @@
-FROM debian:9.5-slim
+FROM alpine:latest
 
 LABEL "com.github.actions.name"="Clubhouse Checker"
 LABEL "com.github.actions.description"="Throws a check failure if the PR wasn't created with the clubhouse helper or doesn't have a clubhouse card in the description"
 LABEL "com.github.actions.icon"="activity"
 LABEL "com.github.actions.color"="yellow"
+
+RUN apk add --no-cache \
+	bash \
+	ca-certificates \
+	coreutils \
+	curl \
+	jq
 
 ADD check_for_clubhouse.sh /check_for_clubhouse.sh
 ENTRYPOINT ["./check_for_clubhouse.sh"]
--- check_for_clubhouse.sh
+++ check_for_clubhouse.sh
@@ -12,17 +12,39 @@ if [[ -z "$GITHUB_REPOSITORY" ]]; then
 	exit 1
 fi
 
+if [[ -z "$REF_FORMAT" ]]; then
+	echo "Set the GITHUB_REF_FORMAT env variable."
+	exit 1
+fi
+
+URI=https://api.github.com
+API_VERSION=v3
+API_HEADER="Accept: application/vnd.github.${API_VERSION}+json; application/vnd.github.antiope-preview+json"
+AUTH_HEADER="Authorization: token ${GITHUB_TOKEN}"
+
 main() {
-  echo ${GITHUB_PR_SOURCE_BRANCH}
-  echo ${GITHUB_PR_BODY}
-  if [[ ${GITHUB_PR_SOURCE_BRANCH} =~ (/ch*(.+)/*)([^,]*) ]]
+	printenv
+	cat $GITHUB_EVENT_PATH
+	# Get the pull request number.
+	NUMBER=$(jq --raw-output .number "$GITHUB_EVENT_PATH")
+
+	echo "running $GITHUB_ACTION for PR #${NUMBER}"
+
+	body=$(curl -sSL -H "${AUTH_HEADER}" -H "${API_HEADER}" "${URI}/repos/${GITHUB_REPOSITORY}/pulls/${NUMBER}")
+	PR_BODY=$(echo "$body" | jq --raw-output .body)
+	echo $PR_BODY
+
+	# check if the branch path has a clubhouse card associated
+  if [[ ${PR_BODY} =~ (\[ch*(.+)\])([^,]*) ]]
   then
-  	echo "Branch name is good."
-  elif [[ ${GITHUB_PR_BODY} =~ (\[ch*(.+)\])([^,]*) ]]
-  then
-  	echo "PR body is good."
+		echo "PR body is good."
+		exit 0
+	elif [[ ${GITHUB_REF} =~ ($REF_FORMAT) ]]
+	then
+		echo "Branch name is good."
+		exit 0
   else
-  	echo "yo dawg, where da clubhouse card at?"
+  	echo "Pull request body and branch name are not of the expected format."
     exit 1
   fi
 }
